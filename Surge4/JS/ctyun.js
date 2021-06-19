9a91041d1302ef35f4349ca39302799af1bc8069
GH3a <64625271+GH3a@users.noreply.github.com>
2021-06-19 12:09:56 +0800

    Delete Surge4/JS directory

--- Surge4/JS/ctyun.js
+++
@@ -1,8 +0,0 @@
-/*
-分享地址：https://t.me/
-https://.*.com//services
-hostname=www.ctyun.cn
-*/
-
-body = $response.body.replace('"pValue":"5"', '"pValue":"20"').replace('"pass":"false"', '"pass":"true"')
-$done({body});
