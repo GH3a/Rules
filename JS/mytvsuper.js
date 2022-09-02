86999f9fc082bd2edec586c806bfdc5717e96749
GH3a <64625271+GH3a@users.noreply.github.com>
2022-09-02 10:08:04 +0800

    Update mytvsuper.js

--- JS/mytvsuper.js
+++ JS/mytvsuper.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body); 
-body.channels.show_with_subscription = "true";
-body.channels.is_4k = "true";
+body.channels['show_with_subscription'] = "true";
+body.channels['is_4k'] = "true";
 body = JSON.stringify(body);
 $done({body}); 
