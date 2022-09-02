31218fe92ce2d1b3565b81026b17ef70305696d6
GH3a <64625271+GH3a@users.noreply.github.com>
2022-09-02 16:44:34 +0800

    Update and rename mytvpaid5.js to mytvpaid6.js

--- JS/mytvpaid6.js
+++ JS/mytvpaid6.js
@@ -2,9 +2,11 @@ let url = $request.url;
 let headers = $request.headers;
 let body = $request.body;
 
-bodyc = body.replace(/free/,"paid");
+
+body.user_type = "paid";
+
 $done({
 	url: url,
 	headers: headers,
-	body: bodyc
+	body: body
 });
