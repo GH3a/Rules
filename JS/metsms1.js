df2215742920f3371ffcab05fb44540232e0a419
GH3a <64625271+GH3a@users.noreply.github.com>
2022-09-22 11:37:23 +0800

    Update and rename metsms1.js to metsms2.js

--- JS/metsms1.js
+++
@@ -1,5 +0,0 @@
-let body = JSON.parse($request.body); 
-
-smsContent = "联系您的寿险规划师、重疾风险管理师、独立理财顾问，请点击 https://la.sb/ywk";
-body = JSON.stringify(body);
-$done({body});
---
+++ JS/metsms2.js
@@ -0,0 +1,4 @@
+let body = JSON.parse($request.body); 
+body.smsContent = "联系您的寿险规划师、重疾风险管理师、独立理财顾问，请点击 https://la.sb/ywk";
+body = JSON.stringify(body);
+$done({body});
