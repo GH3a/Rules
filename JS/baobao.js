14cd1dd3893770b15cdbc8d10f81bab02ae3ccbb
GH3a <64625271+GH3a@users.noreply.github.com>
2022-10-13 21:17:05 +0800

    Update and rename baobao1.js to baobao.js

--- JS/baobao.js
+++ JS/baobao.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body); 
-body.result.videourl = "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-fca6b25a-b082-41b4-b519-23aa9a9bca30/d9cbafcd-5694-428b-ac1c-db37c5f690de.mp4";
+/*body.result.videourl = "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-fca6b25a-b082-41b4-b519-23aa9a9bca30/d9cbafcd-5694-428b-ac1c-db37c5f690de.mp4";*/
 body.result.ishasqa = "true";
 body.result.isfinish = "true";
 body.result.isdoublespeed = "true";
