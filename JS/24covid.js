20876ee733f2c2ef9061a7c9c050de2d50e06bc8
GH3a <64625271+GH3a@users.noreply.github.com>
2022-10-16 01:53:47 +0800

    Update and rename 24covid1.js to 24covid.js

--- JS/24covid.js
+++ JS/24covid.js
@@ -1,5 +1,6 @@
 let body = JSON.parse($response.body); 
 let timenow = body.data.currentTime;
-body.data.testCompleteTime = timenow;
+/*body.data.testCompleteTime = timenow;*/
+body.data.testCompleteTime = "2022-10-15 22:41:54";
 body = JSON.stringify(body);
 $done({body});
