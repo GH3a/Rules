827adeb1126716a0a4823c8ffa384a595cd770bd
GH3a <64625271+GH3a@users.noreply.github.com>
2022-10-17 14:35:10 +0800

    Update 24time.js

--- JS/24time.js
+++ JS/24time.js
@@ -17,10 +17,20 @@ let second = "00";
 /*let second = zeroFill(date.getSeconds());//秒*/
 let curTime = date.getFullYear() + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 
+
+let datetest = new Date(new Date().getTime() - 8 * 44 * 60 * 1000);
+let monthtest = zeroFill(datetest.getMonth() + 1);//月
+let daytest = zeroFill(datetest.getDate());//日
+let hourtest = zeroFill(datetest.getHours());//时
+let minutetest = zeroFill(datetest.getMinutes());//分
+let secondtest = "44";
+/*let second = zeroFill(datetest.getSeconds());//秒*/
+let curTimetest = datetest.getFullYear() + "-" + monthtest + "-" + daytest + " " + hourtest + ":" + minutetest + ":" + secondtest;
+
 let body = JSON.parse($response.body); 
 let listdata = body.data.pageUtils.list;
 for (i in listdata) {
-  listdata[0].testCompleteTime = curTime;
+  listdata[0].testCompleteTime = curTimetest;
   listdata[0].collectionTime = curTime;
 }
 body = JSON.stringify(body);
