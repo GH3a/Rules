9bc5055c4e30359f5fcb44650930d1382666cfd3
GH3a <64625271+GH3a@users.noreply.github.com>
2023-01-27 23:03:05 +0800

    Update pikpakvip.js

--- JS/pikpakvip.js
+++ JS/pikpakvip.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body); 
-body.data.expire = "2099-02-04T22:49:05+08:00";
+body.data.expire = "2099-12-31T23:59:59+08:00";
 body.data.type = "platinum";
 body = JSON.stringify(body);
 $done({body});
