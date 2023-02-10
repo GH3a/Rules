8c4c94b8778b0b9467e16f137c0319ccfbdabb3c
GH3a <64625271+GH3a@users.noreply.github.com>
2023-02-10 19:14:27 +0800

    更新 pikpakvip1.js

--- JS/pikpakvip1.js
+++ JS/pikpakvip1.js
@@ -1,7 +1,7 @@
 let body = JSON.parse($response.body); 
-body.data.status = "valid";
+body.data.status = "ok";
 body.data.expire = "2099-12-31T23:59:59+08:00";
-body.data.fee_record = "2099-12-31T23:59:59+08:00";
+#body.data.fee_record = "2099-12-31T23:59:59+08:00";
 body.data.type = "platinum";
 body = JSON.stringify(body);
 $done({body});
