5ccec9969d7983f4e2621bc3d159d034a2ad9867
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 22:01:41 +0800

    Update and rename r1-2.js to r1.js

--- JS/r1-2.js
+++
@@ -1,3 +0,0 @@
-let body = $response.body; 
-let body = body.replace(/budingkkk@126.com/g, "eatsleepeat@eat.com");
-$done({body});
---
+++ JS/r1.js
@@ -0,0 +1,2 @@
+let body = $response.body.replace(/budingkkk@126.com/g, "eatsleepeat@eat.com");
+$done({body});
