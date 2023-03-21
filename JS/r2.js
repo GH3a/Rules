db9d99d4e776ea066e78aa617a8deae83b0b5484
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 21:17:46 +0800

    Update and rename r2.js to r2-1.js

--- JS/r2.js
+++
@@ -1,7 +0,0 @@
-let body = JSON.parse($response.body); 
-body.data.displayName = "Eatsleepeat";
-body.data.binaries.displayName = "Eatsleepeat";
-body.data.creator.displayName = "Eatsleepeat";
-body.data.emailAddress = "eatsleepeat@eat.com";
-body = JSON.stringify(body);
-$done({body});
