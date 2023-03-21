3a195b220278d24e73cb6fa2ffdd2e12ca97d44d
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 20:30:15 +0800

    Update r1.js

--- JS/r1.js
+++ JS/r1.js
@@ -1,4 +1,4 @@
 let body = JSON.parse($response.body); 
-let body = body.data.email = "eatsleepeat@eat.com";
+let body.data.email = "eatsleepeat@eat.com";
 body = JSON.stringify(body);
 $done({body});
