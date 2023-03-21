dfd92a55572accf759d6cceadd118c02fb34a0ba
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 21:35:37 +0800

    Update r2-2.js

--- JS/r2-2.js
+++ JS/r2-2.js
@@ -1,3 +1,4 @@
 let body = $response.body; 
 let body = body.replace(/"displayName\".+\"externalResourceType\"/g, "\"displayName\": \"Eatsleepeat\",         \"externalResourceType\"");
+let body = body.replace(/budingkkk@126.com/g, "eatsleepeat@eat.com");
 $done({body});
