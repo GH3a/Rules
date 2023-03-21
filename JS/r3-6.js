77a55daf253be018a72c50ea178b3121249bc262
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 22:53:41 +0800

    Update r3-6.js

--- JS/r3-6.js
+++ JS/r3-6.js
@@ -1,4 +1,4 @@
 let headers = $request.headers;
 headers['Cookie'] = "SESS=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786; SID=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786;";
-headers['User-Agent'] = "SketchUp\/1839 CFNetwork\/1404.0.5 Darwin\/22.3.0";
+headers['User-Agent'] = "SketchUp/1839 CFNetwork/1404.0.5 Darwin/22.3.0";
 $done({headers});
