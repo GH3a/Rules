0c40b362d811866db2cb6289c400ea8396c2ce5f
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-22 00:05:19 +0800

    Create r8.js

---
+++ JS/r8.js
@@ -0,0 +1,4 @@
+let headers = $request.headers;
+headers['Authorization'] = "9d197f82606d5ec949a38d80aa56653af0a7b88abab53003d6fda72d5141586a";
+headers['User-Agent'] = "SketchUp/1839 CFNetwork/1404.0.5 Darwin/22.3.0";
+$done({headers});
