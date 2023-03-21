a4eb9f878b9e99839facf8d4f1e0981c100aa38b
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 22:28:28 +0800

    Update r3-1.js

--- JS/r3-1.js
+++ JS/r3-1.js
@@ -1,3 +1,3 @@
 let headers = $request.headers;
-headers.Cookie = "SESS=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786; SID=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786";
+headers.Cookie = "SESS=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786; SID=AuthKey 2d21a0a4-7f8e-4a66-8924-98f924c00786;";
 $done({headers});
