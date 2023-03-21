ad7125aaf0c9b042e8d82cd2d32f9c7691295edc
GH3a <64625271+GH3a@users.noreply.github.com>
2023-03-21 21:06:10 +0800

    Update and rename r1.js to r1-1.js

---
+++ JS/r1-1.js
@@ -0,0 +1,3 @@
+let body = $response.body; 
+let body.replace(/id@yuwaikoon.com/g, "eatsleepeat@eat.com");
+$done({body});
--- JS/r1.js
+++
@@ -1,4 +0,0 @@
-let body = JSON.parse($response.body); 
-let body.data.replace(/id@yuwaikoon.com/g, "eatsleepeat@eat.com");
-body = JSON.stringify(body);
-$done({body});
