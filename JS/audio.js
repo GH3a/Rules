ad83e9ccd4a8996f9c2d678aaa9b2dfcacbc056d
GH3a <64625271+GH3a@users.noreply.github.com>
2023-11-03 23:32:38 +0800

    Update audio.js

--- JS/audio.js
+++ JS/audio.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 
   // 修改div的class，添加 vjs-hidden
-body = body.replace(/<div class="vjs-control-bar" dir="ltr">/g, '<div class="vjs-control-bar vjs-hidden" dir="ltr">');
+body = body.replace(/<div class="vjs-control-bar"/g, '<div class="vjs-control-bar vjs-hidden"');
 
   // 在video标签中添加controls属性
 body = body.replace(/<video id="videoPlayerID_html5_api"/g, '<video id="videoPlayerID_html5_api" controls="controls"');
