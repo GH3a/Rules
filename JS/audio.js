bf6fc1477777bec0566ccb18b58408b26cf18dfe
GH3a <64625271+GH3a@users.noreply.github.com>
2023-11-03 23:21:16 +0800

    Update audio.js

--- JS/audio.js
+++ JS/audio.js
@@ -1,11 +1,10 @@
-window.onload = function() {
-  // 为video元素添加controls属性
-  var video = document.getElementById('videoPlayerID_html5_api');
-  video.setAttribute('controls', 'controls');
+let body = $response.body;
 
-  // 获取class包含'vjs-control-bar'的div元素
-  var controlBar = document.querySelector('div.vjs-control-bar');
+  // 修改div的class，添加 vjs-hidden
+body = body.replace(/<div class="vjs-control-bar" dir="ltr">/g, '<div class="vjs-control-bar vjs-hidden" dir="ltr">');
 
-  // 在该div元素的class中添加'vjs-hidden'
-  controlBar.className += ' vjs-hidden';
-};
+  // 在video标签中添加controls属性
+body = body.replace(/<video id="videoPlayerID_html5_api"/g, '<video id="videoPlayerID_html5_api" controls="controls"');
+
+  // 使用修改后的body覆盖原来的响应体
+$done({ body });
